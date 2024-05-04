f32acad63cc6e1ecb67a2aa0ca6d83a0e76753a1
fyoussef <fillipyuou@gmail.com>
2024-05-04 14:23:40 -0300

    fix type errors on build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   format: ["cjs", "esm"],
   entry: ["./src/index.ts"],
-  // dts: true,
+  dts: true,
   shims: true,
   skipNodeModulesBundle: true,
   clean: true,
